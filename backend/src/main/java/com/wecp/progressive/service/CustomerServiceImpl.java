9f07397941bf9dfaf0c2b40b92bbcc5c0e85ed9f
username <wecp-dummy@gmail.com>
2024-01-27 12:47:20 +0000

    3rd push

--- backend/src/main/java/com/wecp/progressive/service/CustomerServiceImpl.java
+++ backend/src/main/java/com/wecp/progressive/service/CustomerServiceImpl.java
@@ -1,73 +1,79 @@
+
 package com.wecp.progressive.service;
 
-import com.wecp.progressive.dao.CustomerDAO;
-import com.wecp.progressive.entity.Customers;
 
-import java.util.*;
+import com.wecp.progressive.dao.CustomerDAO;
+import com.wecp.progressive.dao.CustomerDAOImpl;
+import com.wecp.progressive.entity.Customers;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
 
-    private static List<Customers> cl = new ArrayList<>();
-
-    private CustomerDAO customerDAO;
+    CustomerDAO customerDAO;
+    private static List<Customers> customersList = new ArrayList<>();
 
     public CustomerServiceImpl(CustomerDAO customerDAO) {
         this.customerDAO = customerDAO;
     }
+
     @Override
     public List<Customers> getAllCustomers() throws SQLException {
-        return null;
+        return customerDAO.getAllCustomers();
     }
 
     @Override
     public Customers getCustomerById(int customerId) throws SQLException {
-        return null;
+        return customerDAO.getCustomerById(customerId);
     }
 
     @Override
     public int addCustomer(Customers customers) throws SQLException {
-        return -1;
+        return customerDAO.addCustomer(customers);
     }
 
     @Override
     public void updateCustomer(Customers customers) throws SQLException {
-
+        customerDAO.updateCustomer(customers);
     }
 
     @Override
     public void deleteCustomer(int customerId) throws SQLException {
-
+        customerDAO.deleteCustomer(customerId);
     }
 
     @Override
     public List<Customers> getAllCustomersSortedByName() throws SQLException {
-        return null;
+        List<Customers> sortedCustomers = customerDAO.getAllCustomers();
+        if (sortedCustomers != null) {
+            Collections.sort(sortedCustomers);
+        }
+        return sortedCustomers;
     }
 
     @Override
     public List<Customers> getAllCustomersFromArrayList() {
-        return cl;
+        return customersList;
     }
 
     @Override
     public List<Customers> addCustomersToArrayList(Customers customers) {
-        cl.add(customers);
-        return cl;
+        customersList.add(customers);
+        return customersList;
     }
 
     @Override
     public List<Customers> getAllCustomersSortedByNameFromArrayList(){
-        List<Customers> sc=cl;
-        Collections.sort(sc);
-        return sc;
+        List<Customers> sortedCustomers = customersList;
+        Collections.sort(sortedCustomers);
+        return sortedCustomers;
     }
 
     @Override
     public void emptyArrayList() {
-        cl=new ArrayList<>();
+        customersList = new ArrayList<>();
     }
 }
