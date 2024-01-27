ac4a30960b8851be04fb087140add7d336edf5cc
username <wecp-dummy@gmail.com>
2024-01-27 11:21:14 +0000

    first push

--- backend/src/main/java/com/wecp/progressive/entity/CreditCard.java
+++ backend/src/main/java/com/wecp/progressive/entity/CreditCard.java
@@ -5,4 +5,23 @@ public class CreditCard {
     private Long id;
     private String cardNumber;
     private String cardHolderName;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getCardNumber() {
+        return cardNumber;
+    }
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+    
 }
--- backend/src/main/java/com/wecp/progressive/entity/Loan.java
+++ backend/src/main/java/com/wecp/progressive/entity/Loan.java
@@ -5,5 +5,25 @@ public class Loan {
     private Long id;
     private String loanType;
     private double amount;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getLoanType() {
+        return loanType;
+    }
+    public void setLoanType(String loanType) {
+        this.loanType = loanType;
+    }
+    public double getAmount() {
+        return amount;
+    }
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    
 
 }
