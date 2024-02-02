58e67fe79ead8b672b6508dd3246046ccd940313
username <wecp-dummy@gmail.com>
2024-02-02 03:39:09 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/service/TransactionServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/TransactionServiceImplJpa.java
@@ -1,66 +1,83 @@
 package com.wecp.progressive.service;
 
+
+import com.wecp.progressive.entity.Accounts;
+import com.wecp.progressive.entity.Transactions;
+import com.wecp.progressive.exception.AccountNotFoundException;
+import com.wecp.progressive.exception.OutOfBalanceException;
+import com.wecp.progressive.exception.WithdrawalLimitException;
+import com.wecp.progressive.repository.AccountRepository;
+import com.wecp.progressive.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.wecp.progressive.entity.Transactions;
-import com.wecp.progressive.repository.AccountRepository;
-import com.wecp.progressive.repository.TransactionRepository;
-
 @Service
 public class TransactionServiceImplJpa implements TransactionService {
 
+    private TransactionRepository transactionRepository;
+    private AccountRepository accountRepository;
     @Autowired
-    TransactionRepository transactionRepository;
-
-    @Autowired
-    AccountRepository accountRepository;
-
-    private static List<Transactions> transactions = new ArrayList<>();
-
-    public TransactionServiceImplJpa(TransactionRepository transactionRepository, AccountRepository accountRepository) {
+    public TransactionServiceImplJpa(TransactionRepository transactionRepository,AccountRepository accountRepository) {
         this.transactionRepository = transactionRepository;
         this.accountRepository = accountRepository;
     }
 
     @Override
-    public List<Transactions> getAllTransactions() throws SQLException {
+    public List<Transactions> getAllTransactions() {
         return transactionRepository.findAll();
     }
 
     @Override
-    public Transactions getTransactionById(int transactionId) throws SQLException {
-        return transactionRepository.findById(transactionId).get();
+    public Transactions getTransactionById(int transactionId) {
+        return transactionRepository.findById(transactionId).orElse(null);
     }
 
     @Override
     public int addTransaction(Transactions transaction) throws SQLException {
+        Accounts account = accountRepository.findByAccountId(transaction.getAccounts().getAccountId());
+        double balance = account.getBalance();
+        if (transaction.getAmount() > 30000 && transaction.getTransactionType().equalsIgnoreCase("debit")) {
+            throw new WithdrawalLimitException("Exceeded the withdrawal limit.");
+        }
+        if (balance < transaction.getAmount() && transaction.getTransactionType().equalsIgnoreCase("debit")) {
+            throw new OutOfBalanceException("Transaction amount exceeds the total balance available in the account.");
+        }
+        if (transaction.getTransactionType().equalsIgnoreCase("credit")) {
+            balance = balance + transaction.getAmount();
+        }
+        else {
+            balance = balance - transaction.getAmount();
+        }
+        account.setBalance(balance);
+        accountRepository.save(account);
         return transactionRepository.save(transaction).getTransactionId();
     }
 
     @Override
-    public void updateTransaction(Transactions transaction) throws SQLException {
-        transactionRepository.findById(transaction.getTransactionId()).map(transactions->{
-            transactions.setAccountId(transaction.getAccountId());
-            transactions.setAmount(transaction.getAmount());
-            transactions.setTransactionDate(transaction.getTransactionDate());
-            transactions.setTransactionType(transaction.getTransactionType());
-            return transactionRepository.save(transactions);
-        });
+    public void updateTransaction(Transactions transaction) {
+        transactionRepository.save(transaction);
     }
 
     @Override
-    public void deleteTransaction(int transactionId) throws SQLException {
+    public void deleteTransaction(int transactionId) {
         transactionRepository.deleteById(transactionId);
     }
 
     @Override
     public List<Transactions> getTransactionsByCustomerId(int customerId) throws SQLException {
-       return null;
+        List<Accounts> accountsList = accountRepository.getAccountsByCustomerCustomerId(customerId);
+        List<Transactions> allTransactions = new ArrayList<>();
+        if (accountsList.isEmpty()) {
+            throw new AccountNotFoundException("No accounts found linked with this customer");
+        }
+        for (Accounts account : accountsList) {
+            List<Transactions> transactionsForAccount = transactionRepository.findByAccountsAccountId(account.getAccountId());
+            allTransactions.addAll(transactionsForAccount);
+        }
+        return allTransactions;
     }
-
 }
